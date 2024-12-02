57b7d0fff8b606f38ace870c0bf516a6d0660e35
GASH962408 <tu_correo@example.com>
2024-12-02 15:36:52 -0500

    Bnaner de iconos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './assets/components/Header';
 import LandingPage from './assets/components/LandingPage';
+import IconBanner from './assets/components/iconBanner';
 
 function App() {
     return (
         <div>
             <Header />
             <LandingPage />
+            <IconBanner />
+
         </div>
     );
 }
